467f096d3955ab950b8165b140ccbb9ed9a78ccb
NRestFzn <nashirresta7@gmail.com>
2025-03-16 21:21:19 +0700

    fix: delete movie

--- controllers/MovieController/delete.php
+++ controllers/MovieController/delete.php
@@ -1,9 +1,7 @@
 <?php
-  include("{$_SERVER['DOCUMENT_ROOT']}/controllers/MovieController/_service.php");
+include("{$_SERVER['DOCUMENT_ROOT']}/controllers/MovieController/_service.php");
 
-  $movie_service = new MovieServices();
-    if(isset($_POST['delete_movie'])) {
-        include("{$_SERVER['DOCUMENT_ROOT']}/helper/base_url.php");
-        $movie_service->deleteMovie($_POST['delete_movie']);
-    }
-?>
+$movie_service = new MovieServices();
+
+include("{$_SERVER['DOCUMENT_ROOT']}/helper/base_url.php");
+$movie_service->deleteMovie($_GET['id']);
