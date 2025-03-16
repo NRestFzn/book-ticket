ab0f7e2263a4a462cc80aaa13ac24fcb13ce9fc5
NRestFzn <nashirresta7@gmail.com>
2025-03-16 20:34:25 +0700

    fix: login and register form style

--- pages/login/index.php
+++ pages/login/index.php
@@ -1,5 +1,5 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
   include("{$_SERVER['DOCUMENT_ROOT']}/controllers/AuthController.php");
   $email = $_POST['email'];
   $password = $_POST['password'];
@@ -12,64 +12,67 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT']; ?>/styles/_global.css">
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT']; ?>/styles/form.css">
-    <title>Login</title>
-  </head>
-  <body>
-    <h1>Login Form</h1>
-    <form onsubmit="submitForm(event)" method="POST" class="login">
 
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" placeholder="ex: john@mail.com" required />
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT']; ?>/styles/_global.css">
+  <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/pages/login/style.css">
+  <title>Login</title>
+</head>
 
-      <label for="password">Password:</label>
-      <div class="password">
+<body>
+  <h1>Login Form</h1>
+  <form onsubmit="submitForm(event)" method="POST" class="login">
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" placeholder="ex: john@mail.com" required />
+
+    <label for="password">Password:</label>
+    <div class="password">
       <input type="password" id="password" name="password" placeholder="ex: johndoe123" required />
       <span id="show-password" onclick="showPassword()">show</span>
-      </div>
+    </div>
 
-      <input type="submit" name="login" value="Login" />
-      <p><a href="<?php $_SERVER['DOCUMENT_ROOT']; ?>/pages/register">Don't have an account?</a></p>
-    </form>
+    <input type="submit" name="login" value="Login" />
+    <p><a href="<?php $_SERVER['DOCUMENT_ROOT']; ?>/pages/register">Don't have an account?</a></p>
+  </form>
 
-    <script>
-        function showPassword() {
-          const showButton = document.getElementById('show-password')
-          const passwordInput = document.getElementById("password");
+  <script>
+    function showPassword() {
+      const showButton = document.getElementById('show-password')
+      const passwordInput = document.getElementById("password");
 
-          showButton.innerText = showButton.innerText === "show" ? "hide" : "show";
+      showButton.innerText = showButton.innerText === "show" ? "hide" : "show";
 
-          passwordInput.type = passwordInput.type === "password" ? "text" : "password";
-        }
+      passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+    }
 
-        function submitForm(event) { 
-            event.preventDefault();
- 
-            const formData = new FormData(event.target);
-            formData.append("login", "true");
-  
-            fetch('', { 
-                method: 'POST', 
-                body: formData 
-            }) 
-            .then(response => response.json()) 
-            .then(data => { 
-                if (data.status === 'ok') { 
-                    window.location.replace(data.redirectUrl);
-                    alert(data.message)
-                } else { 
-                    alert(data.message); 
-                } 
-            }) 
-            .catch(error => { 
-                console.error('Error:', error); 
-                alert('Error submitting form'); 
-            }); 
-        }
-    </script>
-  </body>
+    function submitForm(event) {
+      event.preventDefault();
+
+      const formData = new FormData(event.target);
+      formData.append("login", "true");
+
+      fetch('', {
+          method: 'POST',
+          body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.status === 'ok') {
+            window.location.replace(data.redirectUrl);
+            alert(data.message)
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          alert('Error submitting form');
+        });
+    }
+  </script>
+</body>
+
 </html>
--- pages/register/index.php
+++ pages/register/index.php
@@ -16,37 +16,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/styles/_global.css">
-    <link rel="stylesheet" href="/styles/form.css">
+    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/styles/_global.css">
+    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/pages/register/style.css">
     <title>Register</title>
 </head>
+
 <body>
     <h1>Register Form</h1>
     <form method="POST" onsubmit="submitForm(event)">
         <label for="fullname">Fullname:</label>
         <input type="text" id="fullname" name="fullname" placeholder="ex: john doe" required />
-        
+
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" placeholder="ex: john123" required />
-        
+
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" placeholder="ex: john@mail.com" required />
-        
+
         <label for="password">Password:</label>
         <div class=password>
             <input type="password" id="password" name="password" placeholder="ex: johndoe123" required />
             <span onclick="showPassword('password', 'show-password')" id="show-password">show</span>
         </div>
-        
+
         <label for="confirmpassword">Confirm Password:</label>
         <div>
-        <div class="password">
-            <input type="password" id="confirmpassword" name="confirmpassword" placeholder="ex: johndoe123" required />
-            <span onclick="showPassword('confirmpassword', 'show-confirm-password')" id="show-confirm-password">show</span>
-        </div>
+            <div class="password">
+                <input type="password" id="confirmpassword" name="confirmpassword" placeholder="ex: johndoe123" required />
+                <span onclick="showPassword('confirmpassword', 'show-confirm-password')" id="show-confirm-password">show</span>
+            </div>
         </div>
 
         <input type="submit" name="register" value="Register" />
@@ -55,38 +57,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])) {
 
     <script>
         function showPassword(id, textId) {
-          const showButton = document.getElementById(textId)
-          const passwordInput = document.getElementById(id);
+            const showButton = document.getElementById(textId)
+            const passwordInput = document.getElementById(id);
 
-          showButton.innerText = showButton.innerText === "show" ? "hide" : "show";
+            showButton.innerText = showButton.innerText === "show" ? "hide" : "show";
 
-          passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+            passwordInput.type = passwordInput.type === "password" ? "text" : "password";
         }
 
-        function submitForm(event) { 
+        function submitForm(event) {
             event.preventDefault();
- 
+
             const formData = new FormData(event.target);
             formData.append("register", "true");
-  
-            fetch('', { 
-                method: 'POST', 
-                body: formData 
-            }) 
-            .then(response => response.json()) 
-            .then(data => { 
-                if (data.status === 'success') { 
-                    window.location.replace(data.redirectUrl);
-                    alert(data.message)
-                } else { 
-                    alert(data.message); 
-                } 
-            }) 
-            .catch(error => { 
-                console.error('Error:', error); 
-                alert('Error submitting form'); 
-            }); 
+
+            fetch('', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 'success') {
+                        window.location.replace(data.redirectUrl);
+                        alert(data.message)
+                    } else {
+                        alert(data.message);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Error submitting form');
+                });
         }
     </script>
 </body>
+
 </html>
