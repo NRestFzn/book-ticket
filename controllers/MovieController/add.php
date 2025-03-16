2c2f028b980936f492c6ee3fd1338797e7930a5d
NRestFzn <nashirresta7@gmail.com>
2025-03-16 21:22:22 +0700

    fix: filename on add movie

--- controllers/MovieController/add.php
+++ controllers/MovieController/add.php
@@ -1,11 +1,17 @@
 <?php
-  include("{$_SERVER['DOCUMENT_ROOT']}/controllers/MovieController/_service.php");
+include("{$_SERVER['DOCUMENT_ROOT']}/controllers/MovieController/_service.php");
 
-  $movie_service = new MovieServices();
-    if(isset($_POST['add_movie'])) {
-        include("{$_SERVER['DOCUMENT_ROOT']}/helper/file_handler.php");
-        $fileHandler = new FileHandler();
-        $filePath = "uploads/" . $fileHandler->uploadFile("{$_SERVER['DOCUMENT_ROOT']}/uploads/");
-        $movie_service->addMovie($_POST['title'], $_POST['description'], $_POST['status'], $_POST['seat_amount'], $_POST['ticket_price'], $filePath);
-    }
-?>
+$movie_service = new MovieServices();
+if (isset($_POST['add_movie'])) {
+  include("{$_SERVER['DOCUMENT_ROOT']}/helper/file_handler.php");
+  $fileHandler = new FileHandler();
+  $filePath = $fileHandler->uploadFile("{$_SERVER['DOCUMENT_ROOT']}/uploads/");
+  $movie_service->addMovie(
+    $_POST['title'],
+    $_POST['description'],
+    $_POST['status'],
+    $_POST['seat_amount'],
+    $_POST['ticket_price'],
+    $filePath
+  );
+}
